de1a9537c482d78edcee75ab5922e89e88f03416
BMjerson <74162753@continental.edu.pe>
2026-09-18 20:16:18 -0500

    Agregado modulo para eliminar tareas

---
+++ eliminar.py
@@ -0,0 +1,6 @@
+def eliminar_tarea(lista_tareas, indice):
+    if 0 <= indice < len(lista_tareas):
+        tarea_eliminada = lista_tareas.pop(indice)
+        print(f"🗑️ Tarea eliminada: '{tarea_eliminada}'")
+    else:
+        print("❌ El índice no existe.")
